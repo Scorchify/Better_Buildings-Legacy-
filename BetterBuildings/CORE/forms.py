32ec6ef41f9b4639c7c2dfd579fde649848268eb
Scorchify <139656419+Scorchify@users.noreply.github.com>
2024-07-15 00:08:36 -0400

    good work day :)

--- BetterBuildings/CORE/forms.py
+++ BetterBuildings/CORE/forms.py
@@ -1,5 +1,36 @@
 from django import forms
 
+class InputBox(forms.Textarea):
+    class Media:
+        css = {
+            "all": ["css/input.css"],  
+        }
+
 class ReportingForm(forms.Form):
-    q1 = forms.CharField(widget=forms.Textarea(attrs={"class": "input-box", "placeholder": "Enter your response here!"}))
-    q2 = forms.CharField(widget=forms.Textarea(attrs={"class": "input-box", "placeholder": "Enter your response here!"}))
+    q1 = forms.CharField(
+        widget=InputBox(attrs={
+            "placeholder": "Enter your response here!",
+            "class": "input-box",
+            "rows": 4,  
+            "cols": 50  
+        }),
+        label=''
+    )
+    q2 = forms.ChoiceField(
+        choices=[
+            ('', 'Select'),  # Default option
+            ('Bathrooms', 'Bathrooms'),
+            ('Building', 'Building'),
+            ('Other', 'Other')
+        ],
+        widget=forms.Select(attrs={
+            "class": "select",
+        }),
+        label="How would you categorize your issue?"
+    )
+
+    def clean_q2(self):
+        data = self.cleaned_data['q2']
+        if data == '':
+            raise forms.ValidationError("Please select a valid option.")
+        return data
--- BetterBuildings/CORE/views.py
+++ BetterBuildings/CORE/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReportingForm
+from django.utils import timezone
 
 
 #page requests
@@ -17,6 +18,7 @@ def display(request):
 def reportPosted(request):
     q1_response = None
     q2_response = None
+    report_time: None
     form = ReportingForm()
 
     if request.method == "POST":
@@ -25,7 +27,8 @@ def reportPosted(request):
             # Retrieve cleaned data
             q1_response = form.cleaned_data['q1']
             q2_response = form.cleaned_data['q2']
-            return render(request, "reportPosted.html", {"q1_response": q1_response, "q2_response": q2_response})
+            report_time = timezone.now()
+            return render(request, "reportPosted.html", {"q1_response": q1_response, "q2_response": q2_response, "report_time": report_time})
         
     else:
         form = ReportingForm()
